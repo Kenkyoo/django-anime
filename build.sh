63d0812229e1e3d5623fe02488aea92fedc66aaa
Franco <frfantusatti@protonmail.com>
2026-06-28 20:52:13 -0300

    edit bash

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 
-cd project
+(
+    cd project
+    python manage.py collectstatic --no-input
+    python manage.py migrate
+)
 
-python manage.py collectstatic --no-input
 
-python manage.py migrate
-
-cd ..

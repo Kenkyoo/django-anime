4e7c95540e10b4fdb5900215a8c17c88b9982ea6
Franco <frfantusatti@protonmail.com>
2026-06-28 20:24:42 -0300

    edit template and add Bulmacss

--- project/app/views.py
+++ project/app/views.py
@@ -2,20 +2,29 @@ from django.shortcuts import render
 import requests
 
 def index(request):
-    response = requests.get('https://api.jikan.moe/v4/top/anime')
-    data = response.json()
-    
-    animes = data.get('data', [])[:12]
-
-    anime_list = []
-    for anime in animes:
-        anime_list.append({
-            'title': anime.get('title', 'Sin título'),
-            'synopsis': anime.get('synopsis', 'Sin sinopsis'),
-            'image': anime.get('images', {}).get('jpg', {}).get('image_url', ''),
-            'year': anime.get('year', 'Sin año'),
-            'score': anime.get('score', 'Sin puntuación'),
-            'episodes': anime.get('episodes', 'Desconocido')
+    try:
+        response = requests.get('https://api.jikan.moe/v4/top/anime')
+        response.raise_for_status()
+        data = response.json()
+        
+        animes = data.get('data', [])[:12]  # 12 animes para grid de 3 o 4 columnas
+        
+        anime_list = []
+        for anime in animes:
+            anime_list.append({
+                'title': anime.get('title', 'Sin título'),
+                'synopsis': anime.get('synopsis', 'Sin sinopsis disponible'),
+                'image': anime.get('images', {}).get('jpg', {}).get('image_url', ''),
+                'year': anime.get('year', 'N/A'),
+                'score': anime.get('score', 'N/A'),
+                'episodes': anime.get('episodes', 'N/A'),
+                'mal_id': anime.get('mal_id', ''),
+                'type': anime.get('type', 'N/A'),
+            })
+        
+        return render(request, 'index.html', {
+            'anime_list': anime_list,
+            'total_results': data.get('pagination', {}).get('items', {}).get('total', 0)
         })
-    
-    return render(request, 'index.html', {'anime_list': anime_list})
+    except requests.exceptions.RequestException as e:
+        return render(request, 'index.html', {'error': f'Error al conectar con la API: {str(e)}'})

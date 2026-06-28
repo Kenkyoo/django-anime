be2ad55ee676743d91e2818cd1ebb91f8d450c67
Franco <frfantusatti@protonmail.com>
2026-06-28 19:20:25 -0300

    add views and templates

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def index(request):
+    response = requests.get('https://api.jikan.moe/v4/top/anime')
+    data = response.json()
+    
+    animes = data.get('data', [])[:12]
+
+    anime_list = []
+    for anime in animes:
+        anime_list.append({
+            'title': anime.get('title', 'Sin título'),
+            'synopsis': anime.get('synopsis', 'Sin sinopsis'),
+            'image': anime.get('images', {}).get('jpg', {}).get('image_url', ''),
+            'year': anime.get('year', 'Sin año'),
+            'score': anime.get('score', 'Sin puntuación'),
+            'episodes': anime.get('episodes', 'Desconocido')
+        })
+    
+    return render(request, 'index.html', {'anime_list': anime_list})
